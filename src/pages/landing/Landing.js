4f53d14ba844e9cb4634a84ad519218f9058b0b6
Abhishek <kabhishek18@gmail.com>
2023-05-18 19:19:36 +0530

    First Commit

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import Typewriter from "typewriter-effect";
-import landingImage from "../../images/me.svg";
+import landingImage from "../../images/me.png";
 import Draw from "../../components/Draw";
 import SocialIcons from "../../components/SocialIcons";
 
